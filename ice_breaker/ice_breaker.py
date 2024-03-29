c282c0d8e7c3734bde0e7f6e6285d6357bbf0709
chriswilson2020 <73828727+chriswilson2020@users.noreply.github.com>
2024-03-29 15:36:57 +0100

    update output parser

--- ice_breaker/ice_breaker.py
+++ ice_breaker/ice_breaker.py
@@ -4,6 +4,7 @@ from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from output_parsers import person_intel_parser
 
 name = "Harrison Chase"
 
@@ -18,9 +19,10 @@ def ice_break(name: str) -> str:
         given the LinkedIn information {information} about a person from I want I want you to create:
         1. a short summary
         2. two interesting facts about them
+        \n{format_instructions}
     """
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"], template=summary_template, partial_variables={"format_instructions":person_intel_parser.get_format_instructions()}
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
